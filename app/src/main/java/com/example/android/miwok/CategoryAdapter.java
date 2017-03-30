679374f859b5a339acfe6e04e65f84577722dc63
renzmallari <renzmallari777@yahoo.com>
2017-03-30 11:33:12 -0400

    added labels for each categories on the tab bar

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,9 +10,12 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 
 public class CategoryAdapter extends FragmentPagerAdapter {
+    private Context mContext;
 
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(FragmentManager fm, MainActivity context) {
+
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -30,5 +34,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position == 0){
+            return mContext.getString(R.string.category_numbers);
+        }else if(position == 1){
+            return mContext.getString(R.string.category_family);
+        }else if(position == 2){
+            return  mContext.getString(R.string.category_colors);
+        }else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
